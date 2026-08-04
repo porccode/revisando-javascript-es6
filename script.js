48221dd5677b9244f08b8dcb0672a57348daa40f
Jackson <porccode@gmail.com>
2026-08-04 17:05:01 -0300

    Atualizando o readme para fazer checklist dos estudos e revisão

---
+++ script.js
@@ -0,0 +1,8 @@
+var videogame = ["Switch", "PS4", "XBOX", "3Ds"];
+
+for(var item = 0; item < videogame.length; item++) {
+    console.log(videogame[item]);
+    if(videogame[item] === "PS4") {
+        break;
+    }
+}
