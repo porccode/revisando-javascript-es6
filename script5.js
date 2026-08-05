54ac2ad4923085e0cfc41f1dd4bfba1a18fabe2a
Jackson <porccode@gmail.com>
2026-08-05 12:25:42 -0300

    Acessando classes e elementos pelo DOM

---
+++ script5.js
@@ -0,0 +1,7 @@
+var coresParaParede = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#ff00ff', '#00ffff'];
+
+let i = 0;
+coresParaParede.forEach(function(){
+    console.log(i+1 + ': ' + coresParaParede[i]);
+    i++;
+});
