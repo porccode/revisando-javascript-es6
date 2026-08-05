86b476180e7b4a6bd38b654a7908e838837103d7
Jackson <porccode@gmail.com>
2026-08-05 19:42:13 -0300

    Arrow function - 3

--- function.js
+++ function.js
@@ -4,8 +4,11 @@
 
 // console.log(somar(2, 3));
 
-const multiplica = function(a, b) {
-    return a * b;
-}
+// const multiplica = function(a, b) {
+//     return a * b;
+// }
 
-console.log(multiplica(2, 3));
+// console.log(multiplica(2, 3));
+
+const cumprimetar = nome => `Olá ${nome}`;
+console.log(cumprimetar('João'));
