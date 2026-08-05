d09743cc32ae26368075ddd3d5f107a51ceb6284
Jackson <porccode@gmail.com>
2026-08-05 08:26:57 -0300

    forEach usando arrays corretamente

---
+++ script6.js
@@ -0,0 +1,10 @@
+var cores = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#ff00ff', '#00ffff'];
+
+//let i = 0;
+// cores.forEach(function(item){
+//     console.log(item);
+// })
+
+cores.forEach(function(array){
+    console.log(array);
+})
