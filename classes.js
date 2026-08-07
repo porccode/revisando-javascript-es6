4e91b49df9b3831ee3ed26ccc4c3ae5aa50ca71e
Jackson <porccode@gmail.com>
2026-08-07 01:17:17 -0300

    Classes com sintaxes específicas, com o mesmo molde do Constructor

--- classes.js
+++ classes.js
@@ -1,3 +1,15 @@
-const menu = document.querySelector('.menu');
+function Button(text, backgroun) {
+    this.text = text;
+    this.background = background;
+}
 
-console.log(menu);
+Button.prototype.render = function() {
+    const button = document.createElement('button');
+    button.textContent = this.text;
+    button.style.backgroundColor = this.background;
+    return button;
+}
+
+const blueButton = new Button('Comprar', 'blue');
+
+//primeiro erro!
