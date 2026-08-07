906e0f4e4cbf1afabcf8ccb8f84e37ec87349f69
Jackson <porccode@gmail.com>
2026-08-07 01:46:01 -0300

    Exemplo com callback

---
+++ function-expression.js
@@ -0,0 +1,17 @@
+// const saudacao = function(nome) {
+//   return `Olá, ${nome}`;
+// };
+
+// console.log(saudacao("Ana"));
+
+const numeros = [1, 2, 3];
+
+numeros.forEach(function(item) {
+  console.log(item);
+});
+
+// console.log(somar(2, 3)); // erro
+
+// const somar = function(a, b) {
+//   return a + b;
+// };
